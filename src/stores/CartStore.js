44076e7ce21c12b1d1b61c8c927cb5eabb1bfe9d
eimwe <prokopenkoserge@inbox.ru>
2023-03-20 14:00:33 +0600

    Added state preserver for HMR

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,4 @@
-import { defineStore } from "pinia";
+import { defineStore, acceptHMRUpdate } from "pinia";
 import { groupBy } from "lodash";
 import { useAuthUserStore } from "./AuthUserStore";
 
@@ -55,3 +55,7 @@ export const useCartStore = defineStore("CartStore", {
     },
   },
 });
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useCartStore, import.meta.hot));
+}
